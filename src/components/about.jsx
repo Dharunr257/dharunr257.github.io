c30200bd7bec02796b121779f15047f5d63fcffd
Dharun257 <dharunkumar257@gmail.com>
2026-01-14 18:49:16 +0530

    Refactor code structure for improved readability and maintainability

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -3,7 +3,7 @@ import { FaLinkedin, FaEnvelope } from "react-icons/fa";
 import { FiGithub } from "react-icons/fi";
 import AOS from "aos";
 import "aos/dist/aos.css";
-import profileImage from "/assets/profile.jpg";
+import profileImage from "/assets/profile.png";
 
 const roles = ["Cloud Engineer | AWS Certified Cloud Practitioner (CLF-C02)"];
 
